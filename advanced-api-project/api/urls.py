fce3d3ef1ecb5c8d148e399f610cdbcff46a2a3a
Sofoniyas <sofi123man@gmail.com>
2025-11-29 19:41:40 +0300

    Added unit testings

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from .views import (
     BookListView,
-    BookDetailView,
     BookCreateView,
     BookUpdateView,
     BookDeleteView,
@@ -9,7 +8,6 @@ from .views import (
 
 urlpatterns = [
     path('books/', BookListView.as_view(), name='book-list'),
-    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
     path('books/create/', BookCreateView.as_view(), name='book-create'),
 
     # Updated paths (as required):
