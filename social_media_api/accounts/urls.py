f7eb21fe27a2e8cf4d75dd6ff75251a6d9a5fac4
Sofoniyas <sofi123man@gmail.com>
2025-12-14 02:51:58 +0300

    Add follow/unfollow endpoints and update accounts URL patterns

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path("register/", SignupViewSet.as_view(), name="register"),
     path("login/", ObtainAuthToken.as_view(), name="login"),
     path('follow/<int:user_id>', FollowView.as_view(), name='follow-toggle'),
-    path('unfollow/<int:user_id>', FollowView.as_view(), name='unfollow-toggle'),
+    path('unfollow/<int:user_id>/', FollowView.as_view(), name='unfollow-toggle'),
     path("profile/<str:username>/", ProfileViewSet.as_view(), name="profile"),
 ]
