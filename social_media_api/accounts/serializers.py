3f2b3b9e439f21782af7fdec6c04e93a1d3aa827
Sofoniyas <sofi123man@gmail.com>
2025-12-13 22:22:59 +0300

    Fix signup serializer to meet ALX auth requirements

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -2,36 +2,32 @@ from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 
-User = get_user_model()
 
 class SignupSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
-    password2 = serializers.CharField(write_only=True)
+    password = serializers.CharField()
+    password2 = serializers.CharField()
 
     class Meta:
-        model = User
+        model = get_user_model()
         fields = ('username', 'email', 'password', 'password2')
 
     def validate(self, data):
         if data['password'] != data['password2']:
             raise serializers.ValidationError("Passwords do not match")
-        if User.objects.filter(email=data['email']).exists():
-            raise serializers.ValidationError("Email already registered")
-        if User.objects.filter(username=data['username']).exists():
-            raise serializers.ValidationError("Username already registered")
         return data
 
     def create(self, validated_data):
         validated_data.pop('password2')
         password = validated_data.pop('password')
 
-        user = User.objects.create_user(
+        user = get_user_model().objects.create_user(
             password=password,
             **validated_data
         )
 
         Token.objects.create(user=user)
         return user
+
 
 
 class ProfileSerializer(serializers.ModelSerializer):
