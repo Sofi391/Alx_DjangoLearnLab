879c2ccaf5d1ddf1cb3c87150a8824dd9234aee7
Sofoniyas <sofi123man@gmail.com>
2025-12-14 20:56:18 +0300

    Add Like and Notification features

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -72,7 +72,7 @@ class LikeView(GenericAPIView):
     def post(self, request, pk):
         post = generics.get_object_or_404(Post, pk=pk)
 
-        like, created = Like.objects.get_or_create(user=request.user,post=post)
+        like, created = Like.objects.get_or_create(user=request.user, post=post)
 
         if not created:
             return Response(
